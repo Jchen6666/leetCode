47d63964a79d9587184232aab4881dd05e247dc1
Jerry <cj154014737@hotmail.com>
2022-06-02 12:56:24 +0200

    Update maxBoxWeight.ts

--- maxBoxWeight/maxBoxWeight.ts
+++ maxBoxWeight/maxBoxWeight.ts
@@ -7,12 +7,11 @@ let maxSetAWeight = 0
 
 function maxBoxWeight(curIndex: number, curWeight: number, curSetA: number[]) {
     if (curWeight > totalWeight/2) {
-        const curResSum = curSetA.reduce((previous, cur) => previous + cur, 0)
-        const isBetterResult = (curSetA.length < minimumSizeOfSetA) || (curSetA.length == minimumSizeOfSetA && curResSum > maxSetAWeight)
+        const isBetterResult = (curSetA.length < minimumSizeOfSetA) || (curSetA.length == minimumSizeOfSetA && curWeight > maxSetAWeight)
         if (isBetterResult) {
             minimumSizeOfSetA = curSetA.length
             setA = curSetA.reverse()
-            maxSetAWeight = curResSum
+            maxSetAWeight = curWeight
         }
         return 
     }
